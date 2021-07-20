26cc149255da0132df409d3305a0e9fe25d7ea36
gugapadilha <guga.santospadilha@rede.ulbra.br>
2021-07-20 15:43:22 -0300

    newFunctionsList

--- functions/script.js
+++ functions/script.js
@@ -103,3 +103,73 @@ const filtro = Array.prototype.filter.call(li2, (item) => {
 
 console.log(filtro)
 console.log(li)
+
+
+//FUNCTION.APPLY()
+//O apply(this, [arg1, arg2, ...]) funciona como o call, a única diferença é que os argumentos da função são passados através de uma array.
+
+const numeros3 = [3,4,6,1,34,44,32];
+Math.max.apply(null, numeros3);
+Math.max.call(null, 3, 4, 5, 6, 7, 20);
+
+// Podemos passar null para o valor
+// de this, caso a função não utilize
+// o objeto principal para funcionar
+
+
+//APPLY VS CALL
+//A única diferença é a array como segundo argumento.
+
+const li4 = document.querySelectorAll('li');
+
+function itemPossuiAtivo(item) {
+  return item.classList.contains('ativo');
+}
+
+const filtro1 = Array.prototype.filter.call(li4, itemPossuiAtivo);
+const filtro2 = Array.prototype.filter.apply(li4, [itemPossuiAtivo]); //MESMA COISA
+console.log(li)
+
+//FUNCTION.BIND()
+//Diferente de call e apply, bind(this, arg1, arg2, ...) não irá executar a função mas sim retornar a mesma com o novo contexto de this.
+const $ = document.querySelectorAll.bind(document, 'li');
+
+
+
+//ARGUMENTOS E BIND
+//Não precisamos passar todos os argumentos no momento do bind, podemos passar os mesmos na nova função no momento da execução da mesma.
+
+const carro2 = {
+  marca: 'Ford',
+  ano: 2018,
+  acelerar: function(aceleracao, tempo) {
+    return `${this.marca} acelerou ${aceleracao} em ${tempo} segundos`;
+  }
+}
+
+const honda = {
+  marca: 'Honda'
+}
+
+const acelerarHonda = carro2.acelerar.bind(honda, 100); 
+
+console.log(acelerarHonda(20))
+
+//console.log(carro2)
+
+
+
+//ARGUMENTOS COMUNS
+//Podemos passar argumentos padrões para uma função e retornar uma nova função.
+
+function imc(altura, peso) {
+  return peso / (altura * altura);
+}
+
+const imc180 = imc.bind(null, 1.80); //passando o NULL pois acima é uma funcao pura, pois nao tem 
+//referência a nenhum objeto a nao ser o window, porem, não estou usando o window para fazer nada
+
+imc(1.80, 70); // 21.6
+imc180(70); // 21.6
+
+console.log(imc(1.80, 70))
