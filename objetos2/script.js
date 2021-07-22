7bf3a832457f503411109c751cd5c2c5cf8006c3
gugapadilha <guga.santospadilha@rede.ulbra.br>
2021-07-22 15:32:03 -0300

    finalObjectsJS

--- objetos2/script.js
+++ objetos2/script.js
@@ -1,9 +1,27 @@
 // Crie uma função que verifique
 // corretamente o tipo de dado
 
+function verificar(dado){
+  return Object.prototype.toString.call(dado);
+};
+
+console.log(verificar({}));
+console.log(verificar([]));
+console.log(verificar(''));
+
 // Crie um objeto quadrado com
 // a propriedade lados e torne
 // ela imutável
+
+const quadrado = {}
+Object.defineProperties(quadrado, {
+  lados: {
+    value: 4,
+    enumerable: true,
+  }
+});
+
+console.log(quadrado)
 
 // Previna qualquer mudança
 // no objeto abaixo
@@ -13,6 +31,13 @@ const configuracao = {
   background: '#333'
 }
 
+console.log(Object.isFrozen(configuracao))
+width: 700;
+console.log(configuracao)
+
 // Liste o nome de todas
 // as propriedades do
 // protótipo de String e Array
+
+console.log(Object.getOwnPropertyNames(String.prototype))
+console.log(Object.getOwnPropertyNames(Array.prototype))
